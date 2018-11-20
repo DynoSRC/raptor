acf19bafa7b1f7ea11960462caa09d609115e845
Josh Everett <josh.r.everett@gmail.com>
2018-11-19 19:54:14 -0800

    JSX templates.

---
+++ jsx/ExampleSetView.jsx
@@ -0,0 +1,13 @@
+<ExampleSetView>
+  <div class="derp">{derp}</div>
+  <div class="herp">{herp}</div>
+  <ExampleView class="left"
+               countdown={countdown}
+               foobar={foobar}></ExampleView>
+  <ExampleView class="middle"
+               countdown={countdown}
+               foobar={foobar}></ExampleView>
+  <ExampleView class="right"
+               countdown={countdown}
+               foobar={foobar}></ExampleView>
+</ExampleSetView>
---
+++ jsx/ExampleView.jsx
@@ -0,0 +1,4 @@
+<ExampleView>
+  <div class="countdown">{countdown}</div>
+  <div class="foobar">{foobar}</div>
+</ExampleView>
